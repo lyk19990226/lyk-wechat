18b65ab203a7a3b5b3e527230d64a13ff07d0caf
lyk19990226 <1272198474@qq.com>
2022-02-14 23:54:39 +0800

    视频详情页页面跳转

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -15,18 +15,56 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    getTopMV(0).then(res => {
-      this.setData({topMVs:res.data})
-      console.log(res)
-    })
+    this.getTopMVData(0)
   },
-  onReachBottom(){//监听滚动到底部
-    getTopMV(this.data.topMVs.length).then(res => {
-      if (!this.data.hasMore) return 
-      this.setData({topMVs:this.data.topMVs.concat(res.data)})
-      this.setData({hasMore:res.hasMore})
+  // 封装网络请求方法
+  getTopMVData: async function(offset) {
+    //判断是否可以请求
+    if(!this.data.hasMore && offset !== 0) return 
+
+    //展示加载动画
+    wx.showNavigationBarLoading()
+
+    //真正的请求数据
+    const res = await getTopMV(offset) 
+    let newData = this.data.topMVs
+    if(offset === 0) {
+      newData = res.data
+    }else{
+      newData = newData.concat(res.data)
+    }
+
+    //设置数据
+    this.setData({topMVs:newData})
+    this.setData({hasMore:res.hasMore})
+    wx.hideNavigationBarLoading()
+
+    if(offset === 0 ) {
+      wx.stopPullDownRefresh({
+        success: (res) => {},
+      })
+    }
+  },
+
+  //下拉刷新生命周期
+  onPullDownRefresh: async function(){
+    this.getTopMVData(0)
+  },
+  
+  //上拉加载更多生命周期
+  onReachBottom: async function() {
+    this.getTopMVData(this.data.topMVs.length)
+  },
+
+  // 点击跳转视频播放页面
+  handleVideoItemClick(event){
+    // console.log(event)  
+    // 获取id
+    const id = event.currentTarget.dataset.item.id
+    // console.log(id)
+    // 页面跳转 
+    wx.navigateTo({
+      url:'/pages/detail-video/index?id='+id+'&name=lyk'
     })
-    console.log("到底部了")
   }
 })
-//hasMore  
--- service/api_video.js
+++ service/api_video.js
@@ -1,5 +1,16 @@
 import hyRequest from "./index"
 
-export function getTopMV(offset, limit = 10){
+export function getTopMV(offset, limit = 10){//请求视频页数据
   return hyRequest.get("/top/mv",{offset,limit})//返回Promise
+}
+export function getMVURL(id){//请求播放视频地址
+  return hyRequest.get("/mv/url",{id})
+}
+
+export function getMVDetail(mvid){//MV的详情
+  return hyRequest.get("/mv/detail",{mvid})
+}
+
+export function getRelatedVideo(id){//推荐的相关视频
+  return hyRequest.get("/related/allvideo",{id})
 }
